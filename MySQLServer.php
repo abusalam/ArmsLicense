28726f59016e455c63e59862399ce2031a1a7a42
abusalam <abusalamparvezalam@gmail.com>
2013-05-04 17:31:52 +0530

    Login module included

---
+++ MySQLServer.php
@@ -0,0 +1,10 @@
+<?php 
+define("HOST_Name","localhost");
+define("MySQL_User","root");
+define("MySQL_Pass","mysql");
+define("MySQL_DB","test");
+define("MySQL_Pre","ARMS_");
+define("LifeTime","20");
+define("BaseDIR","/arms/"); // path to the php scripts on the webserver for the menus
+define("AppTitle","Arms License - Paschim Medinipur");
+?>
