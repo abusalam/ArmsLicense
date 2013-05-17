005d4ef544656ed8e61efbbbe6ce863741168346
abusalam <abusalamparvezalam@gmail.com>
2013-05-17 16:50:32 +0530

    Added AppFormData.php css button and jquery min

--- AppForm.php
+++ AppForm.php
@@ -4,12 +4,12 @@ require_once('functions.php');
 FN\session_auth();
 FN\HtmlHeader("Data Entry");
 FN\IncludeCSS();
+FN\IncludeCSS("css/Button.css");
 FN\jQueryInclude();
 ?>
 <script type="text/javascript">
 $(function() {
-	$( ".datepick" ).datepicker({minDate: "-43Y", 
-								maxDate: "-18Y",
+	$( ".datepick" ).datepicker({
 								dateFormat: 'yy-mm-dd',
 								showOtherMonths: true,
 								selectOtherMonths: true,
@@ -34,10 +34,75 @@ $(function() {
 	require_once("topmenu.php");
 	?>
 	<div class="content">
-		<h2>Data Entry</h2>
-		
-		<p>Content goes here</p>
-		
+		<div class="form">
+			<form action="<?php echo FN\GetVal($_SERVER,'PHP_SELF'); ?>" method="post">
+				<div style="text-align: center;"><h2>Form No. III</h2>
+					<h3>(Register)</h3>
+				</div>
+				<span>West Bengal Form No. 139</span><br /><br/><br/>
+				<h3>License Details</h3>
+				<div class="FieldGroup">License No:<input type="text"/></div>
+				<div class="FieldGroup">Issued On:<input class="datepick" type="text"/></div>
+				<div class="FieldGroup">Expiry:<input class="datepick" type="text"/></div>
+				<div class="FieldGroup">Inspection:<input type="text"/></div>
+				<div class="FieldGroup">Purpose:<select name="Purpose">
+						<option>-- Select --</option>
+						<option>Protection</option>
+						<option>Sports</option>
+						<option>Institution</option>
+					</select></div>
+				<div class="FieldGroup">Police Station:<select name="PS">
+						<option>-- Select --</option>
+						<option>ANANDAPUR</option>
+						<option>BELDA</option>
+						<option>CHANDRAKONA</option>
+						<option>DANTAN</option>
+						<option>DASPUR</option>
+						<option>DEBRA</option>
+						<option>GARHBETA</option>
+						<option>GHATAL</option>
+						<option>GOALTORE</option>
+						<option>KESHIARY</option>
+						<option>KESHPUR</option>
+						<option>KHARAGPUR(T)</option>
+						<option>KHARAGPURL(L)</option>
+						<option>LALGARH</option>
+						<option>MOHANPUR</option>
+						<option>NARAYANGARH</option>
+						<option>PINGLA</option>
+						<option>SABANG</option>
+						<option>SALBONI</option>
+					</select></div>
+				<div class="FieldGroup">Valid within:<select name="ValidWithin">
+						<option>-- Select --</option>
+						<option>State</option>
+						<option>District</option>
+						<option>India</option>
+					</select></div>
+				<br/><br/><br/>
+				<h3>Licensee Details</h3>
+				<div class="FieldGroup">Name:<input type="text"/></div>
+				<div class="FieldGroup">Present Address:<input type="text"/></div>
+				<div class="FieldGroup">Permanent Address:<input type="text"/></div>
+				<div class="FieldGroup">Status:<input type="text"/></div>
+				<div class="FieldGroup">Remarks:<input type="text"/></div>
+				<br /><br/><br/>
+				<h3>Arms Details</h3>
+				<div class="FieldGroup">Serial No:<input type="text"/></div>
+				<div class="FieldGroup">Category:<input type="text"/></div>
+				<div class="FieldGroup">Manufacturer:<input type="text"/></div>
+				<div class="FieldGroup">Weight:<input type="text"/></div>
+				<br /><br/><br/>
+				<h3>Ammunition Details</h3>
+				<div class="FieldGroup">Category:<input type="text"/></div>
+				<div class="FieldGroup">Manufacturer:<input type="text"/></div>
+				<div class="FieldGroup">Weight:<input type="text"/></div>
+				<div class="FieldGroup">Quantity:<input type="text"/></div>
+				<div style="clear:both;"></div>
+				<input class="button black" type="submit" value="Update" /> 
+				<input class="button black" type="submit" value="Save" /> 
+			</form>
+		</div>
 	</div>
 	<div class="pageinfo">
 		<?php FN\pageinfo(); ?>
